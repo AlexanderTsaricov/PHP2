a42a956221d4daf0f582b05cf58c3810cbcc49ac
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-04-07 20:13:38 +0500

    fix deamon

--- homework-1/ex1/src/Controllers/DeamonEventsSender.php
+++ homework-1/ex1/src/Controllers/DeamonEventsSender.php
@@ -13,42 +13,30 @@ class DeamonEventsSender extends Command {
         $this->logger = new Logs();
     }
 
-    public function run ($options = []) {
-        $pid = pcntl_fork();
-        if ($pid == -1) {
-            die("Could not fork");
-        } elseif ($pid) {
-            $this->view->send("Parent process exiting");
-            exit();
-        } else {
-            if (posix_setsid() == -1) {
-                die("Could not set session id");
+    public function run($options = []) {
+        $this->logger->write("Worker started with PID: " . getmypid());
+    
+        
+        pcntl_signal(SIGTERM, [$this, 'stopDaemon']);
+        pcntl_signal(SIGINT, [$this, 'stopDaemon']);
+        pcntl_signal(SIGHUP, [$this, 'stopDaemon']);
+    
+        while ($this->running) {
+            pcntl_signal_dispatch();
+    
+            $events = EventHandler::handleEvent();
+            foreach ($events as $event) {
+                $this->logger->write(
+                    date('d.m.y H:i') . " Я отправил сообщение " . $event['text'] . " получателю с id " . $event['receiver']
+                );
             }
-            echo "Child PID: " . getmypid() . "\n";
-            $this->view->send("Daemon started");
-
-            // Выполняем код демона
-            chdir("/");
-
-            fclose(STDOUT);
-            fclose(STDERR);
-            fclose(STDIN);
-
-            pcntl_signal(SIGTERM, [$this, 'stopDaemon']);
-            pcntl_signal(SIGINT, [$this, 'stopDaemon']);
-            pcntl_signal(SIGHUP, [$this, 'stopDaemon']);
-
-            while ($this->running) {
-                pcntl_signal_dispatch();
-                $events = EventHandler::handleEvent();
-                foreach ($events as $event) {
-                    
-                    $this->logger->write(date('d.m.y H:i') . " Я отправил сообщение " . $event['text'] . " получателю с id " . $event['receiver']);
-                }
-                sleep(1);
-            }
+    
+            sleep(1);
         }
+    
+        $this->logger->write("Worker shutting down");
     }
+    
 
     public function stopDaemon() {
         $this->running = false;
